ff57b59d16d608f80efe6b2938e3967efadd9f2d
StevenDelval <asteven59220@free.fr>
2024-10-07 13:24:42 +0200

    body to response_body beceause body is deprecated.

--- exercice_3/main.tf
+++ exercice_3/main.tf
@@ -2,8 +2,8 @@ data "http" "file_to_download" {
   url = "https://cdn.wsform.com/wp-content/uploads/2018/09/country_full.csv"
 }
 
-resource "local_file" "my_file_id" {
+resource "local_file" "file_download" {
   filename = var.file_name
-  content = data.http.file_to_download.body
+  content = data.http.file_to_download.response_body
   file_permission = var.file_permision
 }
