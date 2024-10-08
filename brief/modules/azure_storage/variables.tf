7ff14674eaa8e1ccf4fcf68853cf0fa2f765b3e0
StevenDelval <asteven59220@free.fr>
2024-10-08 14:01:35 +0200

    add vars

---
+++ brief/modules/azure_storage/variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "francecentral"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "rg"
+  description = "The resource group name."
+}
+variable "storage_account_name" {
+  type        = string
+  description = "The storage account name."
+}
+
+variable "storage_container_name" {
+  type        = string
+  description = "The storage container name."
+}
+variable "blob_name" {
+  type        = string
+  description = "The blob name."
+}
