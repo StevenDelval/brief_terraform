3d665429ba0f5d7a41f457c9a6cb82793ed9d3a3
StevenDelval <asteven59220@free.fr>
2024-10-08 16:30:32 +0200

    add vars

--- brief/modules/vm/variables.tf
+++ brief/modules/vm/variables.tf
@@ -2,3 +2,16 @@ variable "username" {
   type        = string
   description = "The username for the local account that will be created on the new VM."
 }
+variable "resource_group_location" {
+  type        = string
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The resource group name."
+}
+variable "network_interface_name" {
+  type        = string
+  description = "The network interface name."
+}
