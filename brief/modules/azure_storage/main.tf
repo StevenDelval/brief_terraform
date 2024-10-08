1ba9179b60a3ae4c4cb27f6423fb97485f10175e
StevenDelval <asteven59220@free.fr>
2024-10-08 14:01:56 +0200

    create storage account and blob

---
+++ brief/modules/azure_storage/main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_storage_account" "storage_account" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.resource_group_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = var.storage_container_name
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "blob" {
+  name                   = var.blob_name
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.storage_container.name
+  type                   = "Block"
+}
