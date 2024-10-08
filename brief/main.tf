60f7edff0d58cff18b05fb1dddee8073460ced37
StevenDelval <asteven59220@free.fr>
2024-10-08 15:30:47 +0200

    add module for web app

--- brief/main.tf
+++ brief/main.tf
@@ -1,10 +1,21 @@
 module "azure_storage"{
     source = "./modules/azure_storage"
 
-    blob_name = var.blob_name
-    storage_account_name = var.storage_account_name
-    storage_container_name = var.storage_container_name
     resource_group_location = var.resource_group_location
     resource_group_name = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    storage_container_name = var.storage_container_name
+    blob_name = var.blob_name
+   
+   
+}
 
+module "web_app" {
+    source = "./modules/web_app"
+
+    resource_group_location = var.resource_group_location
+    resource_group_name = var.resource_group_name
+    app_service_name = var.app_service_name
+    app_service_plan_name = var.app_service_plan_name
+  
 }
