356ed2c64df6bbe4e3700024387883a095c6b132
StevenDelval <asteven59220@free.fr>
2024-10-08 14:11:35 +0200

    add variable

--- brief/variables.tf
+++ brief/variables.tf
@@ -10,6 +10,20 @@ variable "resource_group_name" {
   description = "The resource group name."
 }
 
+variable "storage_account_name" {
+  type        = string
+  description = "The storage account name."
+}
+
+variable "storage_container_name" {
+  type        = string
+  description = "The storage container name."
+}
+variable "blob_name" {
+  type        = string
+  description = "The blob name."
+}
+
 variable "username" {
   type        = string
   description = "The username for the local account that will be created on the new VM."
