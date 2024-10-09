f1178631de2fa0c9bedaa183f6641c85c82fa600
StevenDelval <asteven59220@free.fr>
2024-10-09 14:22:43 +0200

    correct script

--- start_terraform.sh
+++ start_terraform.sh
@@ -29,7 +29,7 @@ if [ $? -eq 0 ]; then
     log_with_date "Storage '$STORAGE_NAME' dans le groupe de ressources '$RESOURCE_GROUP_NAME' créé dans la localisation '$LOCATION'."
 else
     log_with_date "Problème lors de la creation du compte de stockage  '$STORAGE_NAME'."
-    exit
+    exit 1
 fi
 # Creation du Containeur Blob 
 az storage container create --name $NOM_BLOB_CONTAINER --account-name $STORAGE_NAME
@@ -37,7 +37,7 @@ if [ $? -eq 0 ]; then
     log_with_date "Container '$NOM_BLOB_CONTAINER' dans le compte de stockage '$STORAGE_NAME' cree."
 else
     log_with_date "Problème lors de la creation du container '$NOM_BLOB_CONTAINER' dans le compte de stockage '$STORAGE_NAME'."
-    exit
+    exit 1
 fi
 
 cd brief
