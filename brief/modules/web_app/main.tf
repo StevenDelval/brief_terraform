2d0680cf2b2955778976b279372752d2d4a8e357
StevenDelval <asteven59220@free.fr>
2024-10-09 11:25:49 +0200

    fix : correct app_service_plan_id

--- brief/modules/web_app/main.tf
+++ brief/modules/web_app/main.tf
@@ -7,5 +7,5 @@ resource "azurerm_app_service" "webapp" {
   name                = var.app_service_name
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_app_service_plan.existing_asp.id
+  app_service_plan_id = data.azurerm_app_service_plan.existing_asp.id
 }
